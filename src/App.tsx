2947b38e396c9b48bb86dae988962435cf8c12f3
laxman-goud <codewithlaxman92@gmail.com>
2025-12-18 21:53:35 +0530

    feat: mount components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
 import words from "./assets/wordList.json"
+import HangmanDrawing from "./components/HangmanDrawing"
+import HangmanWord from "./components/HangmanWord"
+import Keyboard from "./components/Keyboard"
 
 function App() {
   const [ wordToGuess, setWordToGuess ] = useState<string>(()=>{
@@ -8,8 +11,24 @@ function App() {
   const [ guessedLetters, setGuessedLetters ] = useState<string[]>([])
   
   return (
-    <div>
-      <h1>Hey World!</h1>
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "2rem",
+        margin: "0 auto"
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>
+        Lose Win
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <div style={{alignSelf: "stretch"}}>
+        <Keyboard />
+      </div>
     </div>
   )
 }
