b1202eaf14b7c70c7e24c4baa66fc9b8b64bdb7a
laxman-goud <codewithlaxman92@gmail.com>
2025-12-19 07:17:17 +0530

    feat: add logic to HangmanWord component

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,7 +1,11 @@
 
-const HangmanWord = () => {
-    const word = 'test';
-    const guessedLetters = ['t'];
+type HangmanWordProps = {
+    guessedLetters: string[],
+    wordToGuess: string
+    reveal?: boolean
+}
+
+const HangmanWord = ({guessedLetters, wordToGuess, reveal = false}: HangmanWordProps) => {
 
     return (
         <div
@@ -14,11 +18,12 @@ const HangmanWord = () => {
                 fontFamily: 'monospace',
             }}
         >
-            { word.split("").map((letter, index) => (
+            { wordToGuess.split("").map((letter, index) => (
                 <span style={{borderBottom: ".1em solid black"}} key={index}>
                     <span
                         style={{
-                            visibility: guessedLetters.includes(letter) ? "visible" : "hidden",
+                            visibility: guessedLetters.includes(letter) || reveal ? "visible" : "hidden",
+                            color: !guessedLetters.includes(letter) && reveal ? "red": "black"
 
                         }}
                     >
